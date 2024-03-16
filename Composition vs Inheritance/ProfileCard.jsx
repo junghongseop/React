31457e96e62f9c8fa20a045ec0fa15c5e2f36767
junghongseop <a01082372487@gmail.com>
2024-03-17 00:50:04 +0900

    add :: 컴포지션 예제 코드

---
+++ Composition vs Inheritance/ProfileCard.jsx
@@ -0,0 +1,12 @@
+import Card from "./Card";
+
+function ProfileCard(props) {
+    return (
+        <Card title="hongseop" background="black">
+            <a>안녕하세요</a>
+            <a>저는 리액트를 공부중입니다.</a>
+        </Card>
+    );
+}
+
+export default ProfileCard;
